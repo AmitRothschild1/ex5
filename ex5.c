74be03071a4041c3740edb416a21f50eee3c3867
AmitRothschild1 <amitrot1234@gmail.com>
2025-01-08 17:28:18 +0200

    DONE!

--- ex5.c
+++ ex5.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /*Structs=============================================================================================================*/
 typedef struct Song
 {
@@ -33,6 +32,7 @@ void freePlaylist(Playlist* playlist)
         }
         free(playlist->songs);
     }
+    free(playlist->name);
     free(playlist);
 }
 //
@@ -49,14 +49,14 @@ Playlist* createPlaylist(char* name)
     if (!playlist)
     {
         printf("Failed to allocate memory for Playlist\n");
-        exit(1);
+        return NULL;
     }
     playlist->name = (char*)malloc(strlen(name) + 1);
     if (!playlist->name)
     {
         printf("Failed to allocate memory for playlist name\n");
         free(playlist);
-        exit(1);
+        return NULL;
     }
     strcpy(playlist->name, name);
     playlist->songs = NULL;
@@ -66,6 +66,7 @@ Playlist* createPlaylist(char* name)
 //
 void addPlaylist(Playlist*** playlists, int* playlistsNum, char* name)
 {
+    Playlist* newPlaylist = createPlaylist(name);
     int numOfPlaylists = *playlistsNum;
     if (numOfPlaylists == 0)
     {
@@ -73,9 +74,10 @@ void addPlaylist(Playlist*** playlists, int* playlistsNum, char* name)
         if (!*playlists)
         {
             printf("Failed to allocate memory for playlist array!\n");
-            exit(1);
+            return;
         }
-        (*playlists)[numOfPlaylists] = createPlaylist(name);
+        if(newPlaylist!=NULL)
+        (*playlists)[numOfPlaylists] = newPlaylist;
     }
     else
     {
@@ -84,18 +86,16 @@ void addPlaylist(Playlist*** playlists, int* playlistsNum, char* name)
         {
             printf("Failed to allocate memory for playlists\n");
             freeAllPlaylists(*playlists, *playlistsNum);
-            exit(1);
+            return;
         }
-        *playlists = temp;
-        (*playlists)[numOfPlaylists] = (*playlists)[numOfPlaylists-1];
-        (*playlists)[numOfPlaylists-1] = createPlaylist(name);
+        if(newPlaylist!=NULL)
+        {
+            *playlists = temp;
+            (*playlists)[numOfPlaylists] = (*playlists)[numOfPlaylists-1];
+            (*playlists)[numOfPlaylists-1] = newPlaylist;
+        }
     }
-    /*
-    for (int i = numOfPlaylists; i > 0; i--)
-    {
-        (*playlists)[i] = (*playlists)[i - 1];
-    }
-    */
+    if(newPlaylist!=NULL)
     (*playlistsNum)++;
 }
 //
@@ -139,25 +139,6 @@ void deletePlaylist(Playlist*** playlists, int* playlistsNum ,int deleteIndex) {
     printf("Playlist deleted.\n");
 }
 
-void deletePlaylist1(Playlist*** playlists, int* playlistsNum ,int deleteIndex)//***********************************************************8
-{
-    freePlaylist((*playlists)[deleteIndex-1]);
-    for (int i = deleteIndex - 1; i < *playlistsNum - 1; i++)
-    {
-        playlists[i] = playlists[i + 1];
-    }
-    (*playlistsNum)--;
-        Playlist** temp = (Playlist**)realloc(*playlists,sizeof(Playlist*) * (*playlistsNum));
-        if (!temp)
-        {
-            printf("Memory allocation failed!\n");
-            exit(1);
-        }
-        *playlists = temp;//**********************************************************************************88
-    printf("Playlist deleted.\n");
-}
-
-
 /*Songs_Functions=====================================================================================================*/
 void freeSong(Song* song)
 {
@@ -167,6 +148,7 @@ void freeSong(Song* song)
     free(song->lyrics);
     free(song);
 }
+//
 Song* createSong(char* title, char* artist, int year, char* lyrics)
 {
     Song* song = (Song*)malloc(sizeof(Song));
@@ -204,7 +186,6 @@ void addSong(Playlist* playlist, char* title, char* artist, int year, char* lyri
         if (!playlist->songs)
         {
             printf("Failed to allocate memory for songs\n");
-           // free(playlist->songs);
             exit(1);
         }
     }
@@ -234,43 +215,16 @@ void displaySongs(Playlist* playlist)
         }
 }
 //
-void deleteSong(Playlist* playlist, int deleteIndex) {
-    if (!playlist || playlist->songsNum == 0 || deleteIndex < 1 || deleteIndex > playlist->songsNum) {
+void deleteSong(Playlist* playlist, int deleteIndex)
+{
+    if (!playlist || playlist->songsNum == 0 || deleteIndex < 1 || deleteIndex > playlist->songsNum)
+    {
         printf("Invalid song index.\n");
         return;
     }
-
-    // שחרור כל השדות של השיר לפני מחיקתו
     free(playlist->songs[deleteIndex - 1]->title);
     free(playlist->songs[deleteIndex - 1]->artist);
     free(playlist->songs[deleteIndex - 1]->lyrics);
-    free(playlist->songs[deleteIndex - 1]);  // שחרור השיר עצמו
-
-    // הזזת השירים שמימין לשיר המחוק מקום אחד שמאלה
-    for (int i = deleteIndex - 1; i < playlist->songsNum - 1; i++) {
-        playlist->songs[i] = playlist->songs[i + 1];
-    }
-
-    playlist->songsNum--;
-
-    // טיפול במקרה שבו אין יותר שירים בפלייליסט
-    if (playlist->songsNum == 0) {
-        free(playlist->songs);
-        playlist->songs = NULL;
-    } else {
-        Song** temp = (Song**)realloc(playlist->songs, sizeof(Song*) * playlist->songsNum);
-        if (!temp) {
-            printf("Memory allocation failed!\n");
-            exit(1);
-        }
-        playlist->songs = temp;
-    }
-
-    printf("Song deleted successfully.\n");
-}
-
-void deleteSong1(Playlist* playlist, int deleteIndex)
-{
     free(playlist->songs[deleteIndex - 1]);
 
     for (int i = deleteIndex - 1; i < playlist->songsNum - 1; i++)
@@ -285,36 +239,33 @@ void deleteSong1(Playlist* playlist, int deleteIndex)
     }
     else
     {
-        Song** temp = (Song**)realloc(playlist->songs,sizeof(Song*)*(playlist->songsNum-1));
+        Song** temp = (Song**)realloc(playlist->songs, sizeof(Song*) * playlist->songsNum);
         if (!temp)
         {
             printf("Memory allocation failed!\n");
             exit(1);
         }
-        playlist -> songs = temp;
+        playlist->songs = temp;
     }
     printf("Song deleted successfully.\n");
 }
 /*Input_Functions=====================================================================================================*/
 char* enterNameInput() {
-    char buffer[256]; // קלט בגודל מקסימלי
+    char buffer[256];
 
     if (!fgets(buffer, sizeof(buffer), stdin)) {
         printf("Error reading input.\n");
         exit(1);
     }
 
-    // מחיקת '\n' מסוף הקלט אם קיים
     buffer[strcspn(buffer, "\n")] = '\0';
     buffer[strcspn(buffer, "\r")] = '\0';
 
-    // הקצאת זיכרון דינאמית בהתבסס על האורך של הקלט
     char* name = (char*)malloc(strlen(buffer) + 1);
     if (!name) {
         printf("Memory allocation failed!\n");
         exit(1);
     }
-
     strcpy(name, buffer);
     return name;
 }
@@ -331,8 +282,6 @@ char* enterNameInput1()
         printf("Memory allocation failed!\n");
         exit(1);
     }
-  //  while ((ch = getchar()) == '\n' && (ch = getchar()) != '\r');
-    //while ((ch = getchar()) == '\n');
     while ((ch = getchar()) == '\n' && ch != EOF);
 
     do
@@ -351,8 +300,6 @@ char* enterNameInput1()
         }
         name[length++] = ch;
     }
-    //while ((ch = getchar()) != '\n' && (ch = getchar()) != '\r' && ch != EOF);
-    //while ((ch = getchar()) == '\n' && ch != EOF);
     while ((ch = getchar()) == '\n');
     name[length] = '\0';
     return name;
@@ -461,19 +408,25 @@ void sortSongs(Playlist* playlist)
 
 
 
-int readIntegerInput() {
+int readIntegerInput()
+{
     char buffer[100];
 
-    while (1) {
-        if (!fgets(buffer, sizeof(buffer), stdin)) {
+    while (1)
+    {
+        if (!fgets(buffer, sizeof(buffer), stdin))
+        {
             printf("Error reading input.\n");
             continue;
         }
 
         int value;
-        if (sscanf(buffer, "%d", &value) == 1) {
-            return value;  // קלט תקין, מחזירים את המספר
-        } else {
+        if (sscanf(buffer, "%d", &value) == 1)
+        {
+            return value;
+        }
+        else
+        {
             printf("Invalid input, please enter a number: ");
         }
     }
@@ -493,69 +446,48 @@ void addSongToPlaylist(Playlist* playlists)
     printf("Enter song's details\n");
 
     printf("Title:\n");
-    //title = enterNameInput();
     char *title = enterNameInput();
-    if (title == NULL || strlen(title) == 0) {
+    if (title == NULL || strlen(title) == 0)
+    {
         printf("Error: Invalid playlist name\n");
         free(title);
     }
 
     printf("Artist:\n");
-    //artist = enterNameInput();
     char *artist = enterNameInput();
-    if (artist == NULL || strlen(artist) == 0) {
+    if (artist == NULL || strlen(artist) == 0)
+    {
         printf("Error: Invalid playlist name\n");
         free(artist);
     }
 
     printf("Year of release:\n");
-    //scanf("%d", &year);
     year = readIntegerInput();
 
 
     printf("Lyrics:\n");
     char *lyrics = enterNameInput();
-    if (lyrics == NULL || strlen(lyrics) == 0) {
+    if (lyrics == NULL || strlen(lyrics) == 0)
+    {
         printf("Error: Invalid playlist name\n");
         free(lyrics);
     }
     addSong(playlists,title,artist,year,lyrics);
-
+    free(title);
+    free(artist);
+    free(lyrics);
 }
-
-void clearBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
-
-int getIntInput() {
-    char buffer[256];
-    int number;
-
-    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-        if (sscanf(buffer, "%d", &number) == 1) {
-            return number;
-        }
-    }
-    return -1;  // או ערך ברירת מחדל אחר במקרה של שגיאה
-}
-//
-
 //
 int main()
 {
     Playlist** playlists = NULL;
     int playlistsNum = 0;
     addPlaylist(&playlists, &playlistsNum, "Back to main menu");
-    //
     int option = 0;
     while (option != 4)
     {
         printPlaylistsMenu();
-        //scanf("%d", &option);
-             //   while (getchar() != '\n' && getchar()!='\r');//88888888888888888888888888888888888888888888888888888888
         option = readIntegerInput();
-        //printf("option: %d\n",option);********************************************************************************************
         switch (option)
         {
         case 1:
@@ -564,63 +496,39 @@ int main()
             {
             displayPlaylists(playlists, playlistsNum);
             int playlistChoice;
-           // scanf("%d", &playlistChoice);//88888888888888888888888888888888888888888888888888888888888888888888
             playlistChoice = readIntegerInput();
-            //printf("playlistChoice: %d\n",playlistChoice);*****************************************************************************
-
-
-            //    while (getchar() != '\n' && getchar()!='\r');//88888888888888888888888888888888888888888888888888888888
-            if (playlistChoice == playlistsNum)
+                if (playlistChoice == playlistsNum)
                 break;
             printf("playlist %s:\n",playlists[playlistChoice-1]->name);
                 case1Choice = 0;
             while (case1Choice!=6)
             {
                 printf("\t1. Show Playlist\n\t2. Add Song\n\t3. Delete Song\n\t4. Sort\n\t5. Play\n\t6. exit\n");
-                // scanf("%d", &case1Choice);
                 case1Choice = readIntegerInput();
-                //printf("case1choice: %d\n",case1Choice);***********************************************************************************
-
-                // while (getchar() != '\n' && getchar()!='\r');//88888888888888888888888888888888888888888888888888888888
                 switch (case1Choice)
                 {
-                    //  printf("playlistChoice: %d\n",playlistChoice);
                 case 1:
                     {
                         int songChoice;
                         displaySongs(playlists[playlistChoice-1]);
                         printf("choose a song to play, or 0 to quit:\n");
-                        //  scanf("%d", &songChoice);
                         songChoice = readIntegerInput();
-                        // printf("songchoice: %d\n",songChoice);//**********************************************************
-
-                        //  while (getchar() != '\n' && getchar()!='\r');//88888888888888888888888888888888888888888888888888888888
                         while (songChoice<=playlists[playlistChoice-1]->songsNum && songChoice>0)
                         {
                             playSong(playlists[playlistChoice-1]->songs[songChoice-1]);
                             printf("choose a song to play, or 0 to quit:\n");
-                            //displaySongs(*playlists);
-                            //  scanf("%d", &songChoice);
                             songChoice = readIntegerInput();
-                            //  printf("songchoice: %d\n",songChoice);//***************************************************************************8
-
-                            // while (getchar() != '\n' && getchar()!='\r');//88888888888888888888888888888888888888888888888888888888
                         }
                     }
                     break;
                 case 2:
-                    //addSongToPlaylist(playlists);
                         addSongToPlaylist(playlists[playlistChoice-1]);
                     break;
                 case 3:
                     displaySongs(playlists[playlistChoice-1]);
                     printf("choose a song to delete, or 0 to quit:\n");
                     int deleteIndex;
-                    // scanf("%d",&deleteIndex);
                     deleteIndex = readIntegerInput();
-                    // printf("deleteindex: %d\n",deleteIndex);********************************************************************
-
-                    // while (getchar() != '\n' && getchar()!='\r');//88888888888888888888888888888888888888888888888888888888
                     if (deleteIndex>0 && deleteIndex<=(*playlists)->songsNum)
                         deleteSong(playlists[playlistChoice-1], deleteIndex);
                     break;
@@ -634,7 +542,6 @@ int main()
                     {
                         for (int i = 0; i < playlists[playlistChoice-1]->songsNum; i++)
                         {
-                            //playSong((*playlists)->songs[i]);
                             playSong(playlists[playlistChoice-1]->songs[i]);
                             printf("\n");
                         }
@@ -662,22 +569,11 @@ int main()
         case 3:
             int deleteIndex;
             displayPlaylists(playlists, playlistsNum);
-           // scanf("%d", &deleteIndex);
             deleteIndex = readIntegerInput();
-          //  printf("deleteindex: %d\n",deleteIndex);*********************************************************************************
-              //  while (getchar() != '\n' && getchar()!='\r');//88888888888888888888888888888888888888888888888888888888
             if (deleteIndex>0 && deleteIndex<=playlistsNum-1)
             {
                 deletePlaylist(&playlists,&playlistsNum, deleteIndex);
             }
-            /*
-            else
-            {
-                if(deleteIndex==playlistsNum)
-                {
-                    printf("1. Back to main menu\n");
-                }
-            }*/
             break;
             //
         case 4:
